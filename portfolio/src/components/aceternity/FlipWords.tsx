85f8e8ab1a863ff148f7f6178e39df32171d1280
AhmadMtr <70106976+Ahmad-Mtr@users.noreply.github.com>
2024-07-28 16:01:56 +0000

    page:index2

---
+++ portfolio/src/components/aceternity/FlipWords.tsx
@@ -0,0 +1,17 @@
+import { FlipWords } from './ui/FlipWords'
+import { cn } from "../../utils/cn";
+
+export function FlipWordsDemo() {
+  return (
+    
+      <FlipWords  words={["Web", "Mobile", "UI"]}/>
+    
+  );
+}
+export function FlipWordsConstruction() {
+  return (
+    
+      <FlipWords  words={["construction", "development"]}/>
+    
+  );
+}
